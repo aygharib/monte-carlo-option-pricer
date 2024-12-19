23b443054adf196e6a5eb833fb393f837b2e7386
aygharib <12767433+aygharib@users.noreply.github.com>
2024-12-19 10:03:27 -0500

    remove cuda wrapper lib

--- src/main.cu
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <cuda/api.hpp>
-
-auto main() -> int {
-    auto device_count = cuda::device::count();
-	std::cout << device_count << '\n';
-    std::cout << "hello world\n";
-}
